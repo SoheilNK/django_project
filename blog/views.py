23195edfe7883558b23bb051a8617edc52231407
SoheilNK <sohnajm@gmail.com>
2025-02-10 16:18:33 -0330

    Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Soheil',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted' : 'January 10, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted' : 'January 11, 2025'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
